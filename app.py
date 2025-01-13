8b9f2e142f7c7c2398b9c69aa5083f9a467071d3
quyen274 <162405422+quyen274@users.noreply.github.com>
2025-01-14 02:40:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import os
 with open("scenarios.json", "r", encoding="utf-8") as file:
         scenarios = json.load(file)
 load_dotenv()
-client = OpenAI(api_key="sk-Bz7f8PTHRgg_1oCZg1j741MT-hRPTtb8m9primlM-S1Y0bjQZUearyeqeVZdDlO-npncnh6_UXT3BlbkFJEt_1xHi3QxjJ2cNj5yEV9LrGwlyYcjEcNsAufXK1kO5LjQb8PJKIgaLZbA8Q4Eae05Yii8Ak0A")
+client = OpenAI(api_key="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")
 
 # Load the existing dataset
 current_day_sales = pd.read_csv('current_day_sales.csv')
